056aa0ddc2b757da0a85049d7bd6877fdd52d461
Kyle J. Temkin <ktemkin@binghamton.edu>
2013-01-03 04:22:40 -0500

    Finished the low-level JTAG routines.

--- lib/adept/lowlevel/jtag.rb
+++ lib/adept/lowlevel/jtag.rb
@@ -101,27 +101,257 @@ module Adept
 
 
       #
-      # Sends 
+      # Sends (and recieves) raw data via the JTAG lines.
       #
-      def send_raw(device, tdi_bytes, tms_bytes, bit_count)
+      def self.transmit(device, tms, tdi, bit_count)
+
+        #If TMS and TDI were both provided as byte arrays, send them both.
+        if tms.respond_to?(:size) and tdi.respond_to?(:size)
+
+          #Convert the raw TMS/TDI values into an interleave bytes.
+          interleave = interleave_tms_tdi_bytes(tms, tdi)
+
+          #And perform an interleave transmission
+          transmit_interleave(device, interleave, bit_count)
+
+        #If only TMS was provided as a byte array, use the specialized version of that function.
+        elsif tms.respond_to?(:size)
+          transmit_mode_select(device, tms, tdi, bit_count)
+
+        #If only TDI was provided as a byte array, use the specified version of that function.
+        elsif tdi.respond_to?(:size)
+          transmit_data(device, tms, tdi, bit_count)
+
+        #Otherwise, passively recieve data.
+        else
+          receive(device, tms, tdi, bit_count) 
+        end
 
       end
 
+      #
+      # Tick the Test Clock (TCK) without recieving data.
+      #
+      # device: The device with which to transmit.
+      # tms_value: The static, /boolean/ value (true or false) to be held on TMS while the clock is ticked.
+      # tdi_value: The static, /boolean/ value (true or false) to be held on TDI while the clock is ticked.
+      # tick_count: The amount of times TCK should be ticked.
+      #
+      #
+      def tick(device, tms, tdi, tick_count)
+        ClockTck(device.handle, tms, tdi, bit_count, false)
+      end
+
+      #
+      # Transmits a stream of bits on the TMS (Test Mode Set) line.
+      #
+      # device: The device with which to transmit.
+      # tms: A string (or array) of bytes, which will be transmitted over TMS.
+      # tdi_value: The static, /boolean/ value (true or false) to be held on TDI while the TMS values are transmitted.
+      # bit_count: The total number of bits to be transmitted.
+      #
+      # Returns the values recieved on TDO during the transmission.
+      #
+      def self.transmit_mode_select(device, tms, tdi_value, bit_count)
+
+        #Transmit the given tms values...
+        received = transmit_with(tms) do |send_buffer, receive_buffer| 
+          PutTmsBits(device.handle, tdi_value, send_buffer, receive_buffer, bit_count, false)
+        end
+
+        #... and return the values recieved on TDO.
+        return received
+
+      end
+
+      #
+      # Transmits a stream of bits on the TDI (test data in).
+      #
+      # device: The device with which to transmit.
+      # tms_value: The static, /boolean/ value (true or false) to be held on TMS while the TDI values are transmitted.
+      # tdi: A string (or array) of bytes, which will be transmitted over TDI.
+      # bit_count: The total number of bits to be transmitted.
+      #
+      # Returns the values recieved on TDO during the transmission.
+      #
+      def self.transmit_data(device, tms_value, tdi, bit_count)
+
+        #Transmit the given tms values...
+        received = transmit_with(tdi) do |send_buffer, receive_buffer| 
+          PutTdiBits(device.handle, tms_value, send_buffer, receive_buffer, bit_count, false)
+        end
+
+        #... and return the values recieved on TDO.
+        return received
+
+      end
+
+      #
+      # Transmits a constant pair of TMS/TDI values, and recieves the TDO values that appear.
+      #
+      # device: The device with which to transmit.
+      # tms_value: The static, /boolean/ value (true or false) to be held on TMS while the TD0 values are receieved.
+      # tdi_value: The static, /boolean/ value (true or false) to be held on TDI while the TD0 values are receieved.
+      # bit_count: The total number of bits to be received.
+      #
+      def self.receive(device, tms_value, tdi_value, bit_count)
+      
+        #Determine the number of bytes to be transmitted...
+        receive_bytes = (bit_count / 8.0).ceil
+
+        #Transmit the given tms values...
+        received = transmit_with(nil, receive_bytes) do |send_buffer, receive_buffer| 
+          GetTdoBits(device.handle, tms_value, tdi_value, receive_buffer, bit_count, false)
+        end
+
+        #... and return the values recieved on TDO.
+        return received
+
+      end
+
+
+      #
+      # Sends (and recieves) raw data via the JTAG lines.
+      # Accepts input as an array of _interleaved_ bytes, in the format specified by the DJTG
+      # reference manual.
+      #
+      # device: A reference to a Digilent Adept device.
+      #
+      # interleaved: 
+      #   An array or binary string of single-byte values, in the format specified in the 
+      #   DJTG reference manual. Each byte in the interleaved array should contain a _nibble_ 
+      #   of TMS, and a nibble of TDI, in the following order:
+      #
+      #   TMS[3], TDI[3], TMS[2], TDI[2], TMS[1], TDI[1], TMS[0], TMS[0]
+      #
+      # bit_count: The total amount of bits to send.
+      #
+      def self.transmit_interleave(device, interleave, bit_count)
+
+        #Transmit the given interleave using out transmisison helper function.
+        #
+        #Note that we're expecting to recieve about half as many bits as are contained in the
+        #interleave, as half of them are transmitted on TMS, and the other half on TDI.
+        #
+        receive_data = transmit_with(interleave, interleave.size / 2) do |send_buffer, receive_buffer| 
+          PutTmsTdiBits(device.handle, send_buffer, receive_buffer, bit_count, false)
+        end
+
+        #Return the recieved data.
+        return receive_data
+
+      end
+
+      private
+
+
+      #
+      # Helper function which automatically handles the creation of the send/receive buffers
+      # necessary for JTAG transactions.
+      #
+      # Accepts two arguments:
+      #   transmit_data: The data to be transmitted; will be converted to a C byte array; or nil, if the send_buffer won't be used.
+      #   receive_size: The amount of data to be received. If not provided, the size of transmit_data will be used.
+      #
+      # Requires a block, which should accept two pointers:
+      #   transmit_buffer: A FFI pointer to a block of memory which contains the transmit data.
+      #   receive_buffer: A FFI pointer to a block of memory where the recieved data should be placed.
+      #
+      # Returns the contents of the recieve buffer after the block is called, as a ruby string.
+      #
+      def self.transmit_with(transmit_data, receive_size=nil)
+
+        #If the transmit data was provided as a byte array, convert it to a string of bytes.
+        if transmit_data.respond_to?(:pack)
+          transmit_data = transmit_data.pack('C*').force_encoding('UTF-8')
+        end
+
+        #Create the recieve buffer.
+        #If no receive size was provided, assume the same size as the data to be transmitted.
+        receive_size ||= transmit_data.bytesize
+        receive_buffer = FFI::MemoryPointer.new(receive_size)
+
+        #If transmit data was provided, place it in contiguous memory and get a pointer to it.
+        unless transmit_data.nil? 
+          send_buffer = FFI::MemoryPointer.new(transmit_data.bytesize) 
+          send_buffer.put_bytes(0, transmit_data)
+        end
+
+        #Yield the newly-created send and recieve buffer to the passed-in block.
+        yield send_buffer, receive_buffer
+
+        #And return the contents of the recieve buffer.
+        return receive_buffer.get_bytes(0, receive_size)
+
+      end
+
+
+      #
+      # Helper function, which returns its argument as a byte-string.
+      # Used for converting arrays into byte-strings.
+      #
+      def self.pack_if_necessary(bytes)
+        bytes.respond_to?(:pack) ? bytes.pack('C*').force_encoding('UTF-8') : bytes
+      end
+
+      #
+      # Interleaves two sequences of TMS and TDI values into the format used by the Digilent
+      # API.
+      #
+      # tms: A string (or array of bytes) to be used as TMS values in the interleave.
+      # tdi: A string (or array of bytes) to be used as TDI values in the interleave.
       # 
-      # //overlapped functions
-      # DPCAPI BOOL DjtgPutTdiBits(HIF hif, BOOL fTms, BYTE * rgbSnd, BYTE * rgbRcv, DWORD cbits, BOOL fOverlap);
-      # DPCAPI BOOL DjtgPutTmsBits(HIF hif, BOOL fTdi, BYTE * rgbSnd, BYTE * rgbRcv, DWORD cbits, BOOL fOverlap);
-      # DPCAPI BOOL DjtgPutTmsTdiBits(HIF hif, BYTE * rgbSnd, BYTE * rgbRcv, DWORD cbitpairs, BOOL fOverlap);
-      # DPCAPI BOOL DjtgGetTdoBits(HIF hif, BOOL fTdi, BOOL fTms, BYTE * rgbRcv, DWORD cbits, BOOL fOverlap);
-      # DPCAPI BOOL DjtgClockTck(HIF hif, BOOL fTms, BOOL fTdi, DWORD cclk, BOOL fOverlap);
+      # Returns a byte-string in the format used by transmit_interleave.
       #
+      def self.interleave_tms_tdi_bytes(tms, tdi)
+
+        #Ensure we have two byte arrays of the same length.
+        raise ArgumentError, "You must specify the same amount of bytes for TDI and TMS!" if tdi.size != tms.size
+
+        #If we were given a string-like object, handle it byte by byte.
+        tms = tms.bytes if tms.respond_to?(:bytes)
+        tdi = tdi.bytes if tdi.respond_to?(:bytes)
+
+        #Merge the two arrays into a single array of byte-pairs.
+        byte_pairs = tms.zip(tdi)
+
+        #Convert each of the byte pairs into pairs of interleave bytes.
+        interleave = byte_pairs.map { |tms, tdi| interleave_tms_tdi_byte_pair(tms, tdi) }
+
+        #And flatten the pairs into a long array of interleave bytes.
+        interleave.flatten.pack('C*').force_encoding('UTF-8')
+
+      end
+
       #
+      # Interleaves a single byte of TDI and TMS, creating 
+      #
+      def self.interleave_tms_tdi_byte_pair(tms, tdi)
 
+        #Ensure that the two values we have are represented as numbers.
+        tms = tms.ord
+        tdi = tdi.ord
 
+        #
+        #Perform the interleaves manually; this seems like the most clearly
+        #readable way to do this. This might be a good candidate for optimization
+        #later.
+        #
 
+        lower = \
+          tms[3] << 7 | tdi[3] << 6 | tms[2] << 5 | tdi[2] << 4 | 
+          tms[1] << 3 | tdi[1] << 2 | tms[0] << 1 | tdi[0] << 0
 
+        upper = \
+          tms[7] << 7 | tdi[7] << 6 | tms[6] << 5 | tdi[6] << 4 | 
+          tms[5] << 3 | tdi[5] << 2 | tms[4] << 1 | tdi[4] << 0
+
+      
+        #Return the interleave bytes in little endian order.
+        return lower, upper
+
+      end
     end
-
   end
 end
 
--- spec/lowlevel/jtag_spec.rb
+++ spec/lowlevel/jtag_spec.rb
@@ -14,46 +14,182 @@ include Adept
 #
 describe LowLevel::JTAG do
 
-  #Before each test, create a connection to a Basys board
-  before :each do
-    @device = Device.by_name('Basys2')
+  #
+  # Tests which do not require a connected Basys2 board.
+  #
+  describe "offline functionality" do
+
+    describe ".interleave_tms_tdi_byte_pairs" do
+
+      it "should be able to interleave a single byte of TMS/TDI data" do
+
+        #Since we're testing a private method, test from inside of the module.
+        LowLevel::JTAG.module_eval do 
+
+          #Attempt to interleave the bytes. Note the little endian byte ordering.
+          tms, tdi = 0b1010_1011, 0b1100_1101 
+          LowLevel::JTAG.interleave_tms_tdi_byte_pair(tms, tdi).should == [0b1101_1011, 0b1101_1000]
+
+        end
+
+      end
+
+    end
+
+    describe ".interlace_tms_tdi_byte_pairs" do
+
+      it "should be able to interleave two arrays of bytes into a single string" do
+
+
+        #Since we're testing a private method, test from inside of the module.
+        LowLevel::JTAG.module_eval do 
+
+          tms = [0b1010_1011, 0b1111_0000]
+          tdi = [0b1100_1101, 0b0000_1111]
+
+          string = LowLevel::JTAG.interleave_tms_tdi_bytes(tms, tdi)
+          string.should == [0b1101_1011, 0b1101_1000, 0b0101_0101, 0b1010_1010].pack('C*').force_encoding('UTF-8')
+
+        end
+
+      end
+
+    end
+
   end
 
-  #Close the Basys connection after each test.
-  after :each do
-    @device.close
+  #
+  # Tests which require a connectd Basys2-250K board.
+  # 
+  describe "online functionality" do
+
+    #Before each test, create a connection to a Basys board
+    before :each do
+      @device = Device.by_name('Basys2')
+    end
+
+    #Close the Basys connection after each test.
+    after :each do
+      @device.close
+    end
+
+    describe "pre-connection tests" do
+
+      it "should detect that a Basys2 board supports JTAG" do
+        LowLevel::JTAG.supported?(@device).should be_true
+      end
+
+      it "should detect exactly one JTAG port on a Basys2 board" do 
+        LowLevel::JTAG.port_count(@device).should == 1
+      end
+
+      it "should detect that a Basys2 board supports set the JTAG connection speed" do
+        support = LowLevel::JTAG.supported_calls(@device, 0)
+        support[:set_speed].should be_true
+      end
+
+      it "should detect that a Basys2 board can't set the individual JTAG pins" do
+        support = LowLevel::JTAG.supported_calls(@device, 0)
+        support[:set_pins].should be_false
+      end
+
+      it "should be able to open a JTAG connection to the FPGA on a Basys 2" do
+        LowLevel::JTAG.EnableEx(@device.handle, 0)
+      end
+
+      it "should be able to _close_ a JTAG connection" do
+        LowLevel::JTAG.EnableEx(@device.handle, 0)
+        LowLevel::JTAG.Disable(@device.handle)
+      end
+
+    end
+
+    describe "post-connection tests" do
+
+      #Create a new JTAG connection before each test.
+      before :each do
+        LowLevel::JTAG.EnableEx(@device.handle, 0)
+      end
+
+      #Disconnect from the JTAG device after each test.
+      after :each do
+        LowLevel::JTAG.Disable(@device.handle)
+      end
+
+      #
+      # To test each of the JTAG features below, we request that the devices in the scan chain
+      # identify themselves.
+      # 
+    
+      IDCode_Basys2_250K = "\xD5\x04\x50\x93"
+      IDCode_Platform_Flash = "\x11\xC1\xA0\x93"
+
+      it "should be able to send/recieve using interleaved byte strings" do
+
+        #Request that each of the devices identify themselves.
+        sequence = "\xAA\x22\x00"
+       
+        #Send the command to the device...
+        LowLevel::JTAG.transmit_interleave(@device, sequence, 9)
+
+        #And shift in zeroes, simultaneously recieving the device's ID codes.
+        response = LowLevel::JTAG.transmit_interleave(@device, "\x00" * 16, 64)
+
+        #Check for the correct ID codes.
+        response.reverse.should == IDCode_Platform_Flash + IDCode_Basys2_250K
+
+      end
+
+
+      it "should be able to send/recieve using interleaved arrays of bytes" do
+
+        #Request that each of the devices identify themselves.
+        sequence = [0xAA, 0x22, 0x00] # 1010_1010, 0010_0010, 0000_0000; TMS = 1111 0101
+       
+        #Send the command to the device...
+        LowLevel::JTAG.transmit_interleave(@device, sequence, 9)
+
+        #And shift in zeroes, simultaneously recieving the device's ID codes.
+        response = LowLevel::JTAG.transmit_interleave(@device, "\x00" * 16, 64)
+
+        #Check for the correct ID codes.
+        response.reverse.should == IDCode_Platform_Flash + IDCode_Basys2_250K
+
+      end
+
+      it "should be able to send/recieve using linear arrays of bytes" do
+
+        #Request that each of the devices identify themselves.
+        #tms = [0x5F, 0x00] #1010 1010 =  1111 0101 ; 1010 1111
+        tdi = [0x00, 0x00]
+
+        tms = [0x09, 0x00]
+
+        #Send the command to the device...
+        LowLevel::JTAG.transmit(@device, tms, tdi, 6)
+
+        #And shift in zeroes, simultaneously recieving the device's ID codes.
+        response = LowLevel::JTAG.transmit(@device, "\x00" * 8, "\x00" * 8, 64)
+
+        #Check for the correct ID codes.
+        response.reverse.should == IDCode_Platform_Flash + IDCode_Basys2_250K
+
+      end
+
+      it "should be able to accept more optimized transmit/recieve functions " do
+
+        #Request that each of the devices identify themselves.
+        LowLevel::JTAG.transmit(@device, [0x09, 0x00], false, 6)
+
+        #Recieve the device's ID codes.
+        response = LowLevel::JTAG.receive(@device, false, false, 64)
+
+        #Check for the correct ID codes.
+        response.reverse.should == IDCode_Platform_Flash + IDCode_Basys2_250K
+
+      end
+
+
+    end
   end
-
-
-  it "should detect that a Basys2 board supports JTAG" do
-    LowLevel::JTAG.supported?(@device).should be_true
-  end
-
-  it "should detect exactly one JTAG port on a Basys2 board" do 
-    LowLevel::JTAG.port_count(@device).should == 1
-  end
-
-  it "should detect that a Basys2 board supports set the JTAG connection speed" do
-    support = LowLevel::JTAG.supported_calls(@device, 0)
-    support[:set_speed].should be_true
-  end
-
-  it "should detect that a Basys2 board can't set the individual JTAG pins" do
-    support = LowLevel::JTAG.supported_calls(@device, 0)
-    support[:set_pins].should be_false
-  end
-
-  it "should be able to open a JTAG connection to the FPGA on a Basys 2" do
-    LowLevel::JTAG.EnableEx(@device.handle, 0)
-  end
-
-  it "should be able to _close_ a JTAG connection" do
-    LowLevel::JTAG.EnableEx(@device.handle, 0)
-    LowLevel::JTAG.Disable(@device.handle)
-  end
-
-  
-
-
-
 end
